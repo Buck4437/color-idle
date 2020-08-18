4c271fbcd328ab4bfc32ab83659630c4a4efaee5
Buck4437 <Buck4437@users.noreply.github.com>
2020-08-19 02:09:48 +0800

    options tab

--- js/main/options.js
+++ js/main/options.js
@@ -1,8 +1,76 @@
 const options = {
   save(){
     localStorage.setItem('player', JSON.stringify(player))
-    console.log("Game saved")
+    console.log("Game saved.")
+    clearInterval(saveInterval)
+    saveInterval = setInterval(options.save, 15000)
+  },
+  reset(){
+    if (prompt("Enter RESET in ALL CAPS to reset the game.") === "RESET"){
+      clearInterval(saveInterval)
+      localStorage.clear()
+      window.location.reload()
+    }
+  },
+  import(){
+    let saveStr = prompt("Import your save here:")
+    if (saveStr === null || saveStr === "") return
+    if (isValidSave(saveStr)){
+      clearInterval(saveInterval)
+      let save = window.atob(saveStr)
+      localStorage.setItem('player', save)
+      window.location.reload()
+    }
+    alert("Invalid save!")
+  },
+  export(){
+    let saveStr = window.btoa(JSON.stringify(player))
+    $("#options-export-field").css("display", "inline")
+    $("#options-export-field").val(saveStr);
+    $("#options-export-field").select();
+    try {
+      document.execCommand('copy');
+      $("#options-export-field").css("display", "none")
+      alert("Save copied to clipboard!")
+    } catch (e) {
+       $("#options-export-field").css("display", "none")
+       prompt("Exported Save:", saveStr);
+    }
+  },
+  load(){
+    let save = JSON.parse(localStorage.getItem('player'))
+    if (save === null) return
+    player = saveToDecimal(save, defaultPlayer())
   }
 }
 
-setInterval(options.save, 15000)
+function saveToDecimal(obj, def){
+  let data = obj //merging data from both obj and def together
+  for (let key in def){
+    if (obj[key] === undefined){
+      data[key] = def[key]
+    }
+    else if(typeof obj[key] === "string" && def[key] instanceof Decimal){
+      data[key] = new Decimal(obj[key])
+    }
+    else if(typeof obj[key] === "object" && typeof def[key] === "object"){
+      data[key] = saveToDecimal(obj[key], def[key])
+    }
+    else if(typeof obj[key] !== typeof def[key]){
+      data[key] = def[key]
+    }
+  }
+  return data
+}
+
+function isValidSave(save){
+  try {
+    let parse = JSON.parse(window.atob(save))
+    return true
+  }
+  catch (e){
+    return false
+  }
+}
+
+let saveInterval = setInterval(options.save, 15000)
--- js/main/player.js
+++ js/main/player.js
@@ -1,37 +1,43 @@
-var player = {
-  color:[
-    {
-      amount: new Decimal(0),
-      gainOnReset: new Decimal(0),
-      timer: new Decimal(0),
-      auto: false,
-      highest: new Decimal(0),
-      isUnlocked: true,
-      isUnlockedUpgrade: false,
+const defaultPlayer = function (){
+  return {
+    color:[
+      {
+        amount: new Decimal(0),
+        gainOnReset: new Decimal(0),
+        timer: new Decimal(0),
+        auto: false,
+        highest: new Decimal(0),
+        isUnlocked: true,
+        isUnlockedUpgrade: false,
+      },
+      {
+        amount: new Decimal(0),
+        gainOnReset: new Decimal(0),
+        timer: new Decimal(0),
+        auto: false,
+        highest: new Decimal(0),
+        isUnlocked: false,
+        isUnlockedUpgrade: false,
+      },
+      {
+        amount: new Decimal(0),
+        gainOnReset: new Decimal(0),
+        timer: new Decimal(0),
+        auto: false,
+        highest: new Decimal(0),
+        isUnlocked: false,
+        isUnlockedUpgrade: false,
+      }
+    ],
+    colorUpg: {},
+    stats:{
+      playTime: 0
     },
-    {
-      amount: new Decimal(0),
-      gainOnReset: new Decimal(0),
-      timer: new Decimal(0),
-      auto: false,
-      highest: new Decimal(0),
-      isUnlocked: false,
-      isUnlockedUpgrade: false,
-    },
-    {
-      amount: new Decimal(0),
-      gainOnReset: new Decimal(0),
-      timer: new Decimal(0),
-      auto: false,
-      highest: new Decimal(0),
-      isUnlocked: false,
-      isUnlockedUpgrade: false,
-    }
-  ],
-  colorUpg: {},
-  stats:{
-    playTime: 0
-  },
-  version: [0, 1, 0, 0],
-  lastUpdateTick: Date.now()
+    version: 0,
+    lastUpdateTick: Date.now()
+  }
 }
+
+var player = defaultPlayer()
+
+options.load()
